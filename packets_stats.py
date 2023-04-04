906fad9bba9ef029a9b619a4e7e88720f0ffa1d9
Laaarge <92157305+Laaarge@users.noreply.github.com>
2023-04-04 19:48:10 +0200

    better packet_stats png_packet_stats alot of changes
    Co-authored-by: Quentin Mertens <QuentMe@users.noreply.github.com>

---
+++ packets_stats.py
@@ -0,0 +1,63 @@
+from math import floor
+import pyshark
+import os
+import matplotlib.pyplot as plt
+
+
+#file = input("Enter file name: ")
+file = "appel_22s_Aymeric.pcapng"
+file = "Launch_openConvs_Quit_2_aymeric.pcapng"
+
+bin_size = .1 # in ms
+
+
+capture = pyshark.FileCapture("WireShark_Records/" + file, display_filter="(udp || tcp) && !(ip.src == 192.168.1.38) && !(eth.src == 8c:dc:d4:38:2a:a2) && !(ip.src == 192.168.1.29) && !(ipv6.src == fe80::ba27:ebff:fe6b:b6aa) && !(ip.dst == 239.255.255.250) && !(ipv6.src == fe80::aa6a:bbff:fe81:1883) ")
+capture.load_packets()
+print("File: " + str(file) + " has " + str(len(capture)) + " packets")
+udp_times = []
+tcp_times = []
+dns_times = []
+mdns_times = []
+tls_times = []
+if (capture.__len__() > 0):
+    time_start = float(capture[0].sniff_timestamp)
+    time_end = float(capture[capture.__len__() - 1].sniff_timestamp)
+    for packet in capture:
+        if packet.transport_layer == "UDP":
+            udp_times.append(float(packet.sniff_timestamp) - time_start)
+        elif packet.transport_layer == "TCP":
+            tcp_times.append(float(packet.sniff_timestamp) - time_start)
+        elif packet.transport_layer == "DNS":
+            dns_times.append(float(packet.sniff_timestamp) - time_start)
+        elif packet.transport_layer == "MDNS":
+            mdns_times.append(float(packet.sniff_timestamp) - time_start)
+        elif packet.transport_layer == "TLS":
+            tls_times.append(float(packet.sniff_timestamp) - time_start)
+            
+nBins = floor((time_end-time_start)/bin_size)
+
+# graph dns packets per second
+plt.hist(dns_times, bins=nBins, label="DNS Packets", alpha=1)
+
+# graph mdns packets per second
+plt.hist(mdns_times, bins=nBins, label="MDNS Packets", alpha=1)
+
+# graph tcp packets per second
+plt.hist(tcp_times, bins=nBins, label="TCP Packets", alpha=0.5)
+
+# graph udp packets per second
+plt.hist(udp_times, bins=nBins, label="UDP Packets", alpha=0.5)
+
+# graph tls packets per second
+plt.hist(tls_times, bins=nBins, label="TLS Packets", alpha=1)
+
+
+
+plt.xlabel('Time (s)')
+plt.ylabel('Number of Packets')
+plt.title(file + "\nbin size: " + str(bin_size) + "s")
+
+plt.legend(loc='upper right')
+plt.show()
+
+    
---
+++ png_packet_stats.py
@@ -0,0 +1,65 @@
+from math import floor
+import pyshark
+import os
+import matplotlib.pyplot as plt
+
+
+bin_size = .1 # in s
+
+for file in os.listdir("WireShark_Records/"):
+    capture = pyshark.FileCapture("WireShark_Records/" + file, display_filter="(udp || tcp) && !(ip.src == 192.168.1.38) && !(eth.src == 8c:dc:d4:38:2a:a2) && !(ip.src == 192.168.1.29) && !(ipv6.src == fe80::ba27:ebff:fe6b:b6aa) && !(ip.dst == 239.255.255.250) && !(ipv6.src == fe80::aa6a:bbff:fe81:1883) ")
+    capture.load_packets()
+    print("")
+
+    if (capture.__len__() > 0):
+        time_start = float(capture[0].sniff_timestamp)
+        time_end = float(capture[capture.__len__() - 1].sniff_timestamp)
+        nBins = floor((time_end-time_start)/bin_size)
+        print("Analysing file: " + str(file) + "\n" + str(len(capture)) + " unfilteredd packets and " + str(nBins) + " bins with a filterd duration of: " + str(time_end-time_start)[0:5]+ "s")
+        print("")
+
+        udp_times = []
+        tcp_times = []
+        dns_times = []
+        mdns_times = []
+        tls_times = []
+        for packet in capture:
+            if packet.transport_layer == "UDP":
+                udp_times.append(float(packet.sniff_timestamp) - time_start)
+            elif packet.transport_layer == "TCP":
+                tcp_times.append(float(packet.sniff_timestamp) - time_start)
+            elif packet.transport_layer == "DNS":
+                dns_times.append(float(packet.sniff_timestamp) - time_start)
+            elif packet.transport_layer == "MDNS":
+                mdns_times.append(float(packet.sniff_timestamp) - time_start)
+            elif packet.transport_layer == "TLS":
+                tls_times.append(float(packet.sniff_timestamp) - time_start)
+                
+        
+
+        # graph dns packets per second
+        plt.hist(dns_times, bins=nBins, label="DNS Packets", alpha=1)
+
+        # graph mdns packets per second
+        plt.hist(mdns_times, bins=nBins, label="MDNS Packets", alpha=1)
+
+        # graph tcp packets per second
+        plt.hist(tcp_times, bins=nBins, label="TCP Packets", alpha=0.5)
+
+        # graph udp packets per second
+        plt.hist(udp_times, bins=nBins, label="UDP Packets", alpha=0.5)
+
+        # graph tls packets per second
+        plt.hist(tls_times, bins=nBins, label="TLS Packets", alpha=1)
+
+
+
+        plt.xlabel('Time (s)')
+        plt.ylabel('Number of Packets')
+        plt.title(file + "\nbin size: " + str(bin_size) + "s")
+
+        plt.legend(loc='upper right')
+
+        # save graph
+        plt.savefig("Graphs/" + file[0:-7] + ".png")
+        plt.clf()
