fe44ab0527819a2f29fa9c3a1fb96b9b1a0f9eb3
Laaarge <92157305+Laaarge@users.noreply.github.com>
2023-04-11 20:03:10 +0200

    add Hidden records

--- Packet_length_grapher.py
+++ Packet_length_grapher.py
@@ -1,13 +1,13 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-Packet_lengths = ["0-19","20-39","40-79","80-159","160-319","320-639","640-1279","1280-2559","2560-5119","5120 and greater"]
+Packet_lengths = ["0-39","40-79","80-159","160-319","320-639","640-1279","1280 and greater"]
 
 #* Normal Call
-percentages_normal = [0.00, 0.00, 0.08, 8.00, 8.34, 1.90, 81.67, 0.00, 0.00, 0.00]
+percentages_normal =    [0.00, 0.08, 8.00, 8.34, 1.90, 81.67, 0.00]
 
 #* Audio Only Call
-percentages_audioOnly = [0.00, 0.00, 0.74, 28.08, 67.98, 0.00, 3.20, 0.00, 0.00, 0.00]
+percentages_audioOnly = [0.00, 0.74, 28.08, 67.98, 0.00, 3.20, 0.00]
 
 
 # set width of bar
@@ -15,7 +15,7 @@ barWidth = 0.25
 fig = plt.subplots(figsize =(12, 8))
 
 # Set position of bar on X axis
-br1 = np.arange(10)-barWidth/2
+br1 = np.arange(len(Packet_lengths))-barWidth/2
 br2 = [x + barWidth for x in br1]
 
 
@@ -25,10 +25,14 @@ plt.bar(br2, percentages_normal, width=barWidth, label='video call')
 
 plt.xlabel('Packet Lengths (bytes)', fontweight ='bold', fontsize=15)
 plt.xticks([r for r in range(len(Packet_lengths))],
-        Packet_lengths, rotation=10, size=8)
+        Packet_lengths, rotation=10, fontsize=15)
 
 
 plt.ylabel('Percentage of Packets',fontweight ='bold', fontsize=15)
-plt.legend(loc='upper right')
+plt.title('Packet Length Distribution', fontsize=30)
+plt.yticks(fontsize=15)
+plt.legend(loc='upper left', fontsize=15)
+
+
 plt.savefig("Graphs/" + "Packet_distribution" + ".png")
 #plt.show()
